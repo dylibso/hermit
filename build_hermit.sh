059d611c1dff2452d72d6ffa1a86c0bf9db44333
Gavin Hayes <gavin@dylibso.com>
2023-09-12 18:17:16 -0400

    build: move more hacks out wamr

--- build_hermit.sh
+++ build_hermit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export CC=cosmocc
 export CXX=cosmoc++
-cmake -DWAMR_BUILD_INTERP=1 -DWAMR_BUILD_FAST_INTERP=1 -B build
+cmake -DWAMR_BUILD_INTERP=1 -DWAMR_BUILD_FAST_INTERP=1 -DWAMR_DISABLE_STACK_HW_BOUND_CHECK=1 -DWAMR_BUILD_AOT=0 -B build
 cmake --build build
