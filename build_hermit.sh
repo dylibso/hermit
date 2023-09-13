bf7a5c6e5d7cfad0e938cf27cb83bf6ea150f721
Gavin Hayes <gavin@dylibso.com>
2023-09-13 17:24:05 -0400

    chore: switch to using wamr cosmopolitan platform, bump wamr

--- build_hermit.sh
+++ build_hermit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 export CC=cosmocc
 export CXX=cosmoc++
+rm -rf build
+mkdir build
 cmake -DWAMR_BUILD_INTERP=1 -DWAMR_BUILD_FAST_INTERP=1 -B build
-cmake --build build
+cmake --build build -j
