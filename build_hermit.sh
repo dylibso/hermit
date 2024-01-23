b0b245f2967392fc4050973f154e9fc7f0e523d1
Gavin Hayes <gavin@dylibso.com>
2024-01-23 15:26:36 -0500

    fix: adapt to cosmocc changesm, only use x86_64 cosmo for now

--- build_hermit.sh
+++ build_hermit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-export CC=cosmocc
-export CXX=cosmoc++
+export CC=x86_64-unknown-cosmo-cc
+export CXX=x86_64-unknown-cosmo-c++
 rm -rf build hermit-cli/build hermit-cli/target
 mkdir build
 cmake -DWAMR_BUILD_INTERP=1 -DWAMR_BUILD_FAST_INTERP=1 -B build
